78fc99bf191a71634a70699a29517362a779f3ef
Abizrh <abizarah857@gmail.com>
2024-08-17 22:57:04 +0700

    chore: add jsdocs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,25 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * Class-based          | Tag-based           | Description
+ * -------------------- | ------------------- | -----------
+ * .class               | class="class"       | Adds a class to an element
+ * .class.class         | class="class class" | Adds multiple classes to an element
+ * @param inputs - The input classes
+ * @returns The merged classes
+ * @example
+ * cn("class", "class2") // returns "class class2"
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+/**
+ * Get the language based on the file extension
+ * @param fileName - The file name
+ * @returns The language
+ */
 export const getLanguage = (fileName: string) => {
   const extension = fileName.split(".").pop()?.toLowerCase();
   switch (extension) {
@@ -19,6 +34,9 @@ export const getLanguage = (fileName: string) => {
   }
 };
 
+/**
+ * Initializes IndexedDB
+ */
 export const initializeIndexedDB = () => {
   const request = indexedDB.open("NeovimSimulatorDB", 1);
 
